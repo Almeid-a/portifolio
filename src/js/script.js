29aea5131fe29240d1b96a0f4a26ed80f9a464af
Almeida <gabriel.almeidarod99@gmail.com>
2023-04-15 21:04:09 -0300

    final 3.0

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,17 @@ const navMenu = document.querySelector('header .navegacao-primaria');
 
 
 menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-x')
-    navMenu.classList.toggle('ativado')
-})
+    menu.classList.toggle('fa-x');
+    navMenu.classList.toggle('ativado');
+  
+    // Desativa o menu se algum botão for selecionado
+    if (navMenu.classList.contains('ativado')) {
+      const menuItems = navMenu.querySelectorAll('a');
+      menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+          menu.classList.remove('fa-x');
+          navMenu.classList.remove('ativado');
+        });
+      });
+    }
+  });
