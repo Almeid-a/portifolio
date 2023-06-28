5cea760fce8f755b81f7a2e20827ea67a6a04e4b
Almeid-a <gabriel.alemidarod99@gmail.com>
2023-06-28 20:36:21 -0300

    final

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,58 @@ menu.addEventListener('click', () => {
       });
     }
   });
+
+  var nomeElement = document.querySelector(".nome");
+  var digitandoElement = document.querySelector(".digitando");
+  
+  var textoCompletoNome = nomeElement.innerHTML;
+  
+  nomeElement.innerHTML = "";
+  
+  var indiceNome = 0;
+  
+  function exibirCaractereNome() {
+    nomeElement.innerHTML += textoCompletoNome.charAt(indiceNome);
+    indiceNome++;
+    if (indiceNome < textoCompletoNome.length) {
+      setTimeout(exibirCaractereNome, 50);
+    } else {
+      setTimeout(iniciarDigitando, 500); 
+    }
+  }
+  
+  function iniciarDigitando() {
+    digitandoElement.style.visibility = "visible";
+  
+    var textoCompletoDigitando = digitandoElement.innerHTML;
+  
+    digitandoElement.innerHTML = "";
+  
+    var indiceDigitando = 0;
+  
+    function exibirCaractereDigitando() {
+      digitandoElement.innerHTML += textoCompletoDigitando.charAt(indiceDigitando);
+      indiceDigitando++;
+      if (indiceDigitando < textoCompletoDigitando.length) {
+        setTimeout(exibirCaractereDigitando, 50);
+      } else {
+        iniciarBarraPiscando();
+      }
+    }
+  
+    exibirCaractereDigitando();
+  }
+  
+  function iniciarBarraPiscando() {
+    digitandoElement.innerHTML += '<span class="barra-piscando">|</span>';
+    setInterval(piscarBarra, 500); // Define um intervalo de 500ms para piscar a barra
+  }
+  
+  function piscarBarra() {
+    var barraElement = document.querySelector(".barra-piscando");
+    barraElement.style.visibility = barraElement.style.visibility === "hidden" ? "visible" : "hidden";
+  }
+  
+  digitandoElement.style.visibility = "hidden";
+  
+  exibirCaractereNome();
